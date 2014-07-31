3b5b841ad1864694a482c2c63695735762ae4b1e
Anh Thao PHAM <anhthao.pham@claroline.net>
2014-07-31 12:08:31 +0200

    Implements user synchronization system

--- ClarolineRemoteUserSynchronizationBundle.php
+++ ClarolineRemoteUserSynchronizationBundle.php
@@ -12,9 +12,17 @@
 namespace Claroline\RemoteUserSynchronizationBundle;
 
 use Claroline\CoreBundle\Library\PluginBundle;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
 class ClarolineRemoteUserSynchronizationBundle extends PluginBundle
 {
+    public function getConfiguration($environment)
+    {
+        $config = new ConfigurationBuilder();
+
+        return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'remote-user-synchronization');
+    }
+
     public function hasMigrations()
     {
         return false;
